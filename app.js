a5230ca26913b49d2706faea7aa147d591d52e93
posi23 <andyade23@gmail.com>
2021-11-11 02:43:06 -0600

    created a logo at the top left of the home page and created a css file to style the logo; made some changes to the server file regarding serving static files

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict'
+
 
 // import express module for http connection
 const express = require('express');
@@ -11,11 +11,13 @@ const port = 3030;
 
 app.use('/', bodyParser.urlencoded({ extended: true }));
 
+
+
 app.get('/', (req, res) => {
       res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.use('/', express.static(path.join(__dirname, "pages")));
+app.use(express.static(path.join(__dirname, "pages")));
 
 app.listen(port, () => {
       console.log(`listening on server ${port}`);
