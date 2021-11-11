17947bfa65737e87565be9ed3924c2cc648060b7
posi23 <andyade23@gmail.com>
2021-11-11 00:54:18 -0600

    installed nodemon module; and set up my server to display the first webpage saying 'HELLO WORLD'

--- app.js
+++ app.js
@@ -3,5 +3,20 @@
 // import express module for http connection
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
+
+const port = 3030;
+
+app.use('/', bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+      res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.use('/', express.static(path.join(__dirname, "pages")));
+
+app.listen(port, () => {
+      console.log(`listening on server ${port}`);
+});
